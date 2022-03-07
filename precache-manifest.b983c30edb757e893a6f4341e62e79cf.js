92702ea02504a53e1d6e4c19c8a215e7dd6e2ddc
elginp4tt <elginpatt@u.nus.edu>
2022-03-07 19:06:04 +0800

    Updates

---
+++ precache-manifest.b983c30edb757e893a6f4341e62e79cf.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e283474f73b377d255c28af99b0b5c4d",
+    "url": "/orac/index.html"
+  },
+  {
+    "revision": "e1dc98009f2b53c70310",
+    "url": "/orac/static/css/main.5674f204.chunk.css"
+  },
+  {
+    "revision": "2ee094ca5f7c3f50d12f",
+    "url": "/orac/static/js/2.8218a670.chunk.js"
+  },
+  {
+    "revision": "e1dc98009f2b53c70310",
+    "url": "/orac/static/js/main.7ac33078.chunk.js"
+  },
+  {
+    "revision": "b5af0f77694b4fa8fe90",
+    "url": "/orac/static/js/runtime-main.f59910ec.js"
+  },
+  {
+    "revision": "8d6d9e4e685790d95547b3df8ba4bbf1",
+    "url": "/orac/static/media/lower_ankle.8d6d9e4e.png"
+  },
+  {
+    "revision": "250a1a4a09227ea9420d6317436155ae",
+    "url": "/orac/static/media/lower_foot.250a1a4a.png"
+  },
+  {
+    "revision": "85c76ff4d703dde0d2a1179f94f0dd6e",
+    "url": "/orac/static/media/lower_hip.85c76ff4.png"
+  },
+  {
+    "revision": "69bf88e1a539010bde9be6687427640c",
+    "url": "/orac/static/media/lower_knee.69bf88e1.png"
+  },
+  {
+    "revision": "ba789b2c57edcb5cb28517b5ca6f71bc",
+    "url": "/orac/static/media/skeleton.ba789b2c.png"
+  },
+  {
+    "revision": "97800b2d31788f0f4297a1f7e9efd3a6",
+    "url": "/orac/static/media/spine_cervical.97800b2d.png"
+  },
+  {
+    "revision": "b3ae6d2b003f032c80dbb9a8cece30ec",
+    "url": "/orac/static/media/spine_cervical_lumbar.b3ae6d2b.png"
+  },
+  {
+    "revision": "a1bb826f01f355a1e38fa23bc8574a86",
+    "url": "/orac/static/media/spine_coccyx.a1bb826f.png"
+  },
+  {
+    "revision": "b0993884281bcfb1ebfdc976ae37a045",
+    "url": "/orac/static/media/spine_lumbar.b0993884.png"
+  },
+  {
+    "revision": "5376a8740480edfeb4184c2259244425",
+    "url": "/orac/static/media/spine_sacrum.5376a874.png"
+  },
+  {
+    "revision": "c77a244684970fa2cef8ac99f4729802",
+    "url": "/orac/static/media/spine_whole.c77a2446.png"
+  },
+  {
+    "revision": "d3b5532cbe7873a4330eeffa0815a83c",
+    "url": "/orac/static/media/upper_elbow.d3b5532c.png"
+  },
+  {
+    "revision": "060e5d1349d5bcf7725ad50937f50034",
+    "url": "/orac/static/media/upper_finger.060e5d13.png"
+  },
+  {
+    "revision": "a96f874eeb58081194c8867d413a517d",
+    "url": "/orac/static/media/upper_hand.a96f874e.png"
+  },
+  {
+    "revision": "777c0281f31f73e869edfc1b50d91adb",
+    "url": "/orac/static/media/upper_shoulder.777c0281.png"
+  },
+  {
+    "revision": "2fe85513ad9e9a67579628f128cabf42",
+    "url": "/orac/static/media/upper_thumb.2fe85513.png"
+  },
+  {
+    "revision": "24376150b0a9306d33a66f0f6674628a",
+    "url": "/orac/static/media/upper_wrist.24376150.png"
+  }
+]);
